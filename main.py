f54ae9cb9edb24997db2aa9b16d3c1a526512906
aisaasghari <aisaasghari@gmail.com>
2023-05-24 15:00:34 +0330

    Last Bugs Fixed 🩹

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import openai
 import pyttsx3
-import time
 import speech_recognition as sr
 
 openai.api_key = ""
 
 engine = pyttsx3.init()
+
 
 def translate_audio_to_text(filename):
     recognizer = sr.Recognizer()
@@ -16,10 +16,11 @@ def translate_audio_to_text(filename):
     except:
         print("An Error Occured!")
 
+
 def generate_response(prompt):
     reponse = openai.Completion.create(
-        engine = "text-davinci-003",
-        prompt = prompt,
+        engine="text-davinci-003",
+        prompt=prompt,
         max_tokens=4000,
         n=1,
         stop=None,
@@ -27,26 +28,30 @@ def generate_response(prompt):
     )
     return reponse["choices"][0]["text"]
 
+
 def speak_text(text):
     engine.say(text)
     engine.runAndWait()
 
+
 def main():
-    while True:
-        print("Please Say Ghorbani to Listen your Quesion:")
-        with sr.Microphone() as source:
-            recognizer = sr.Recognizer()
-            audio = recognizer.listen(source)
-            try:
-                transcript = recognizer.recognize_google(audio)
-                if transcript.lower() == "ghorbani":
+    print("Please Say Hello to Listen your Quesion:")
+    with sr.Microphone() as source:
+        recognizer = sr.Recognizer()
+        audio = recognizer.listen(source)
+        transcript = recognizer.recognize_google(audio)
+        if transcript.lower() == "hello":
+            while True:
+                try:
                     filename = "input.wav"
                     print("Say Your Question...")
                     with sr.Microphone() as source:
                         recognizer = sr.Recognizer()
                         source.pause_threshold = 1
-                        audio = recognizer.listen(source,phrase_time_limit=None,timeout=None)
-                        with open(filename,"wb") as f:
+                        audio = recognizer.listen(
+                            source, phrase_time_limit=None, timeout=None
+                        )
+                        with open(filename, "wb") as f:
                             f.write(audio.get_wav_data())
 
                     text = translate_audio_to_text(filename)
@@ -57,8 +62,8 @@ def main():
                         print("ChatGPT Said: {response}")
 
                         speak_text(response)
-            except Exception as e:
-                print("An Error Occured")
+                except Exception as e:
+                    print("An Error Occured")
 
-if __name__ == "__main__":
-    main
+
+main()
